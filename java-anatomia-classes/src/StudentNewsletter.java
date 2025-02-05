63e678c63d0d982db83730901ad0e4b04d48b38a
Rizzi <carlosadmsnt@hotmail.com>
2025-02-05 00:26:31 -0300

    Anatomia das Classes - 5

---
+++ java-anatomia-classes/src/StudentNewsletter.java
@@ -0,0 +1,14 @@
+public class StudentNewsletter 
+{
+    public static void main(String[] args) 
+    {
+        int finalAverage = 6;
+
+        if (finalAverage < 6) 
+            System.out.print("FAILED!");
+        else if (finalAverage == 6)
+            System.out.print("RECOVERY!");
+        else
+            System.out.print("APPROVAD!");        
+    }
+}
