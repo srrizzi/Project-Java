1a059d2a5fbed853342ebdbb3753e29c4251b298
Rizzi <carlosadmsnt@hotmail.com>
2025-02-04 23:06:19 -0300

    Anatomia das Classes 3

--- java-anatomia-classes/src/MyClass.java
+++ java-anatomia-classes/src/MyClass.java
@@ -1,8 +1,27 @@
-public class MyClass {
-    public static void main(String[] args) {
+public class MyClass 
+{
+    public static void main (String[] args) 
+    {
         // System.out.print("Hello class, welcome!");
 
-        final String BR = "Brazil!";
-        System.out.print(BR);
+        // final String BR = "Brazil!";
+        // System.out.print(BR);
+
+        // var
+        // String myName = "Rizzi";
+        // int yearManufacture = 1996;
+        // boolean isTrue = true;
+
+        // yearManufacture = 2025;
+
+        String name = fullName("Carlos", "Rizzi");
+
+        System.out.print(name);
+        
+    }
+
+    public static String fullName (String FirstName, String SecondName)
+    {
+        return "Method result: ".concat(FirstName.concat(" ").concat(SecondName));
     }
 }
