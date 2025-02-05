477ab8c17be83a5357f5b19845138b1f23b65aae
Rizzi <carlosadmsnt@hotmail.com>
2025-02-04 22:37:07 -0300

    Anatomia das classes 2

--- java-anatomia-classes/src/MyClass.java
+++ java-anatomia-classes/src/MyClass.java
@@ -1,5 +1,8 @@
 public class MyClass {
     public static void main(String[] args) {
-        System.out.print("Hello class, welcome!");
+        // System.out.print("Hello class, welcome!");
+
+        final String BR = "Brazil!";
+        System.out.print(BR);
     }
 }
