24cb1017020347a9da65cacd8846b01a8e75a512
Rizzi <carlosadmsnt@hotmail.com>
2025-02-12 02:12:07 -0300

    Finalizado a anatomia de classes

--- java-anatomia-classes/src/edu/AprendendoSintaxeJava/TerminalArgument/AboutMe.java
+++ java-anatomia-classes/src/edu/AprendendoSintaxeJava/TerminalArgument/AboutMe.java
@@ -1,16 +1,29 @@
 package edu.AprendendoSintaxeJava.TerminalArgument;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe
 {
     public static void main(String[] args)
     {
-        String name = args[0];
-        String lastName = args[1];
-        int age = Integer.valueOf(args[2]);
-        double height = Double.valueOf(args[3]);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+        System.out.println("Enter your first name:");
+        String name = scanner.next();
+
+        System.out.println("Enter your last name:");
+        String lastName = scanner.next();
+
+        System.out.println("Enter your age:");
+        int age = Integer.parseInt(scanner.next());
+
+        System.out.println("Enter your height:");
+        double height = Double.parseDouble(scanner.next());
+
+        System.out.println("============================================================");
         System.out.println("Hello, my name is " + name + " " + lastName);
-        System.out.println("I'm " + age + "years old!");
-        System.out.println("My height is " + height + "cm");
+        System.out.println("I'm " + age + " years old!");
+        System.out.println("My height is " + height + " cm");
     }    
 }
